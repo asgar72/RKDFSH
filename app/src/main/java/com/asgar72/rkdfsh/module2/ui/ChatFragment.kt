4afb64921518149a7703e8cce7399ba82667e981
asgar72 <72asgar72@gmail.com>
2024-03-16 22:50:06 +0530

    chat desgin

--- app/src/main/java/com/asgar72/rkdfsh/module2/ui/ChatFragment.kt
+++ app/src/main/java/com/asgar72/rkdfsh/module2/ui/ChatFragment.kt
@@ -6,14 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.asgar72.rkdfsh.R
+import com.asgar72.rkdfsh.databinding.FragmentChatBinding
 
 
 class ChatFragment : Fragment() {
+    private var _binding:FragmentChatBinding?=null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false)
+        _binding =FragmentChatBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 }
